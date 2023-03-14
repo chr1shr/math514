24b9fb89c85341924f2907e6ed4bbe94ace29800
Chris Rycroft <chr@seas.harvard.edu>
2023-03-14 11:02:29 -0400

    Fix comment typos.

--- calculus/pi_simpsons.py
+++ calculus/pi_simpsons.py
@@ -3,8 +3,8 @@ from math import pi,sqrt
 
 # Pi Day code to compute pi with the Simpson's rule
 
-# Integrand that corresponds to a change of variable on calculating the error
-# of a circle
+# Integrand that corresponds to a change of variable on calculating the area of
+# a circle
 def f(y):
     yy=y*y
     return 8*yy*sqrt(2-yy)
@@ -28,7 +28,7 @@ def simp(f,a,b,n):
 j=1
 while j<=4096:
 
-    # Compute Simpson's rule integral with 
+    # Compute Simpson's rule integral
     s=simp(f,0,1,j)
 
     # Print result and absolute error
