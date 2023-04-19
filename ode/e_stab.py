1c8f6b0f7626f09a5c6e2b060681fd6a83a2ed9e
Chris Rycroft <chr@seas.harvard.edu>
2023-04-19 10:30:00 -0500

    ODE examples.

--- ode/e_stab.py
+++ ode/e_stab.py
@@ -4,11 +4,13 @@ from math import exp
 # Initial variables and constants
 y=1
 t=0
-h=0.2
-lam=-3
+h=0.1
 
-# Apply Euler step until t>2
-while t<=2:
+# Choose the constant in the ODE, dy/dt=-lam*y. We need -2=<h*lam=<0 for stability.
+lam=-21
+
+# Apply forward Euler step until t>1
+while t<=1:
 
     # Analytical solution
     yexact=exp(lam*t)
